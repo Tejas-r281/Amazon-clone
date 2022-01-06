49f4f32aa19a4cbd5b9c72a5cc91e6e9df549e9c
Raushan <raushan.043.kumar@gmail.com>
2022-01-06 13:53:00 +0530

    firebase change

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyCxBykfbVLzz7ST4Rp4vHHqAVlPvp9xjqA",
-  authDomain: "clone-92420.firebaseapp.com",
-  projectId: "clone-92420",
-  storageBucket: "clone-92420.appspot.com",
-  messagingSenderId: "739036661278",
-  appId: "1:739036661278:web:73e5f838f554e9fd63100f",
-  measurementId: "G-EGR744XNK1",
+  apiKey: "AIzaSyCKNJVyyJhbC6PnX2IW4uUgnnkFuyTNqys",
+  authDomain: "clone-ec40f.firebaseapp.com",
+  projectId: "clone-ec40f",
+  storageBucket: "clone-ec40f.appspot.com",
+  messagingSenderId: "583992145166",
+  appId: "1:583992145166:web:edced427bb2377646d535f",
+  measurementId: "G-GTGNMXF0WJ",
 };
 
 const firebaseApp= firebase.initializeApp(firebaseConfig);
